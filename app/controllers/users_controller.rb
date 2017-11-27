861dc9c799fc845513472d16a61c2059cadb3e75
Rodrigo Lodeiro <rodrigo.lodeiro@live.com>
2017-11-27 01:43:47 +0000

    worked on User_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,4 +17,27 @@ else
   redirect to '/signup'
 end
 
+get '/login' do
+  if !logged_in?
+    erb :'users/login'
+  else
+    redirect to '/giftees'
+  end
+end
+
+post '/login' do
+  user = User.find_by(params[:username])
+  if user && user.authenticate(params[:password])
+    session[:user_id] = user.id
+    redirect to '/giftees'
+  else
+    redirect to '/login'
+  end
+end
+
+get '/logout' do
+  session.clear
+  redirect to '/login'
+end
+
 end
