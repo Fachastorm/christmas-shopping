65c9efbc4d20ee578488412380dd61f6271199cc
Rodrigo Lodeiro <rodrigo.lodeiro@live.com>
2017-11-27 01:59:26 +0000

    working on GifteeController routes

---
+++ app/controllers/giftees_controller.rb
@@ -0,0 +1,41 @@
+class GifteeController < ActiveRecord::Base
+
+get '/giftees' do
+  if logged_in?
+  @giftee = Giftee.all
+erb :'giftees/index'
+else
+  redirect to '/login'
+end
+
+get '/giftees/new' do
+  if logged_in?
+    erb :'giftees/new'
+  else
+    redirect to '/login'
+  end
+end
+
+post '/giftees' do
+    if @giftee = current_user.giftees.create(params)
+      redirect to ("/giftees/#{@giftee.id}")
+    else
+      redirect to '/giftees/new'
+    end
+  end
+
+get '/giftees/:id' do
+  if logged_in?
+    @giftee = Giftee.find_by(params[:id])
+    if @giftee.user_id = current_user.id
+      erb :'giftees/show'
+    else
+      redirect to '/giftees'
+    end
+  else
+    redirect to '/login'
+  end
+end
+
+
+end
